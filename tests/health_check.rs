a984a3040f23f233128214ec6cd491ae26be8fcb
vickyone0 <vigneshs0.618@gmail.com>
2025-04-27 15:32:19 +0530

    unit test 2

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,6 +2,8 @@ use std::net::TcpListener;
 
 
 
+
+
 fn spawn_app() -> String {
 
     let listener =TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
@@ -34,3 +36,51 @@ assert!(response.status().is_success());
 assert_eq!(Some(0), response.content_length());
 }
 // Launch our application in the background ~somehow~
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_for_invalid_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(400, response.status().as_u16(), "{}", error_message);
+    }
+
+    
+}
